152aaab8ca418ee49cda5d6ec982cb5b81b2a49e
gekambar <gekambar@GEKAMBAR-N94RC.partnet.cisco.com>
2019-02-08 20:24:07 +0530

    updated create lead page

--- src/main/java/pages/CreateLeadPage.java
+++ src/main/java/pages/CreateLeadPage.java
@@ -2,21 +2,24 @@ package pages;
 
 import org.openqa.selenium.WebElement;
 
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.When;
 import wdMethods.ProjectMethods;
 
 public class CreateLeadPage extends ProjectMethods{
-
+	@And("enter company name (.*)")
 	public CreateLeadPage typeCompanyName(String data) {
 		WebElement eleCompanyName = locateElement("id", "createLeadForm_companyName");
 		type(eleCompanyName, data);
 		return this;
 	}
-
+	@And("enter first name (.*)")
 	public CreateLeadPage typeFirstName	(String data) {
 		WebElement eleFirstName = locateElement("id", "createLeadForm_firstName");
 		type(eleFirstName, data);
 		return this;
 	}
+	@And("enter last name (.*)")
 	public CreateLeadPage typeLastName	(String data) {
 		WebElement eleLastName = locateElement("id", "createLeadForm_lastName");
 		type(eleLastName, data);
@@ -28,7 +31,7 @@ public class CreateLeadPage extends ProjectMethods{
 		type(setEmail,data);
 		return this;
 	}
-
+	@When("clicks on create lead")
 	public ViewLeadsPage clickCreateLead() {
 		WebElement eleCreateLead= locateElement("class", "smallSubmit");
 		click(eleCreateLead);
